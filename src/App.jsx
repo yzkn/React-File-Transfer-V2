3354c602c0b0a171849c3d276ebe8458291d96a4
YA <9039417+yzkn@users.noreply.github.com>
2025-07-05 23:56:04 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid';
 
 import FileSelector from './FileSelector.jsx';
 
@@ -9,11 +8,7 @@ function App() {
     <>
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
-          <Grid container alignItems="center" justify="center">
-            <Grid item size={4} offset={4} sx={{ mt: "64px", mb: "64px" }}>
-              <FileSelector />
-            </Grid>
-          </Grid>
+          <FileSelector />
         </CardContent>
       </Card>
     </>
